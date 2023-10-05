8c0320621b502fbba6b39f128e72b4795cb0319d
M-Abdulhakim <143715405+M-Abdulhakim@users.noreply.github.com>
2023-10-05 12:29:02 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,65 @@
+// Opdracht 1
+console.log("Opdracht 1");
+for (let counter = 0; counter < 10; counter++) {
+    console.log(counter);;
+}
+console.log("Einde programma");
+
+// Opdracht 2
+console.log("Opdracht 2");
+for (let counter = 2; counter <= 20; counter++) {
+    console.log(counter);;
+}
+console.log("Einde programma");
+
+console.log("Opdracht 3:");
+for (let i = 2; i <= 20; i++) {
+  console.log(i);
+}
+console.log("Einde programma");
+
+console.log("Opdracht 4:");
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+}
+console.log("Einde programma");
+
+console.log("Opdracht 5:");
+for (let i = 40; i >= 4; i--) {
+  console.log(i);
+}
+console.log("Einde programma");
+
+console.log("Opdracht 6:");
+for (let i = 0; i < 8; i++) {
+  console.log("Ik ontwikkel apps");
+}
+console.log("Einde programma");
+
+console.log("Opdracht 7:");
+for (let i = 0; i <= 12; i++) {
+  console.log("Dit is loop: " + i);
+}
+console.log("Einde programma");
+
+console.log("Opdracht 8:");
+for (;;) {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (i === 9) {
+      break;
+    }
+  }
+  break;
+}
+console.log("Einde programma");
+
+console.log("Opdracht 15:");
+for (let i = 0; i < 20; i++) {
+  if (i < 10) {
+    console.log("Dit is zin 1");
+  } else {
+    console.log("Dit is zin 2");
+  }
+}
+console.log("Einde programma");
